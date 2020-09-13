f1ef5c4ca7ff39195de5e72640a6bfe6668aaaea
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2020-09-13 22:12:52 +0545

    Updates

---
+++ precache-manifest.c355da92ef5046804ef5341d75e3c031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1b2620bd447d8c0d3f62f4a7d9e539",
+    "url": "/grid/index.html"
+  },
+  {
+    "revision": "01ae753f29f7e2e63b7d",
+    "url": "/grid/static/css/main.638d61d5.chunk.css"
+  },
+  {
+    "revision": "1022229c95ef5cb34444",
+    "url": "/grid/static/js/2.4f6ac2c7.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/grid/static/js/2.4f6ac2c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ae753f29f7e2e63b7d",
+    "url": "/grid/static/js/main.c6d9a7b2.chunk.js"
+  },
+  {
+    "revision": "d185a5451e08b102c3cd",
+    "url": "/grid/static/js/runtime-main.55fc4496.js"
+  }
+]);
